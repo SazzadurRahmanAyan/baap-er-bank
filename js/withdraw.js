aa7c83c3104a24ebfe665516fa1d689ec472c904
Your NameSazzadurRahmanAyan <sazzadurrahman.ayan@outlook.com>
2022-08-17 19:47:32 +0600

    relocate some withdraw steps

--- js/withdraw.js
+++ js/withdraw.js
@@ -19,15 +19,19 @@ const withdrawField = document.getElementById('withdraw-field');
 const newWithdrawAmountString = withdrawField.value;
 const newWithdrawAmount = parseFloat(newWithdrawAmountString);
 // console.log(newWithdrawAmount);
+
+// step-7
+withdrawField.value = '';
+
+if(isNaN(newWithdrawAmount)){
+    alert('Please provide a valid number');
+    return;
+}
 // step-3
 const withdrawTotalElement = document.getElementById('withdraw-total');
 const previousWithdrawTotalString = withdrawTotalElement.innerText;
 const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
 // console.log(previousWithdrawTotal);
-
-// step-4
-const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-withdrawTotalElement.innerText = currentWithdrawTotal;
 
 // step-5
 const balanceTotalElement = document.getElementById('balance-total');
@@ -35,10 +39,18 @@ const previousBalanceTotalString = balanceTotalElement.innerText;
 const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 // console.log(previousBalanceTotal);
 
+
+if(newWithdrawAmount > previousBalanceTotal){
+    alert('Baap er bank e eto taka nai');
+    return;
+}
+
+// step-4
+const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+withdrawTotalElement.innerText = currentWithdrawTotal;
+
 // step-6
 const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
 balanceTotalElement.innerText = newBalanceTotal;
 
-// step-7
-withdrawField.value = '';
 })
