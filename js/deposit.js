195160fc4d64ac7c7dbb4deed65064b489b3a6ac
Your NameSazzadurRahmanAyan <sazzadurrahman.ayan@outlook.com>
2022-08-17 18:36:55 +0600

    set deposit total

---
+++ js/deposit.js
@@ -0,0 +1,15 @@
+// step-1: add event listener to the deposit button
+document.getElementById('btn-deposit').addEventListener('click', function(){
+    // console.log('deposit button clicked');
+    // step-2: get the deposit  amount from the deposit input field
+    // for  input  field  use .value to the value inside the input field
+    const depositField = document.getElementById('deposit-field');
+    const depositAmount = depositField.value;
+    // console.log(depositAmount);
+    // step-3: get the current deposit total
+    // for non-input(element other than input, textarea) use innerText to ge the text
+    const depositTotalElement = document.getElementById('deposit-total');
+    const depositTotal = depositTotalElement.innerText;
+    // console.log(depositTotal);
+    depositTotalElement.innerText = depositAmount;
+})
